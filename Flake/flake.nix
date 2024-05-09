8c42833115dc4c30cc948b4c0bb58f993362d018
toxx <txx1337@proton.me>
2024-05-09 15:55:29 +0200

    broken home manager

--- Flake/flake.nix
+++ Flake/flake.nix
@@ -4,11 +4,15 @@
   # Inputs: Sources for packages specified in outputs
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager }:
     let
-      system = "x86_64-linux"
+      system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
@@ -18,9 +22,18 @@
       nixosConfigurations = {
         # profile 
         nixos = lib.nixosSystem {
-          inerhit system;
-          modules = [ ./configuration.nix ];
-        }
-      }
-    };
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.toxx = {
+                imports = [ ./home.nix ];
+              };
+            } 
+          ];
+        };
+      };
+    }; 
 }
